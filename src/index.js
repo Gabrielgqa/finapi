b2896d70024965d0837e239f6f47575a604d4389
GabrielGQA <gabrielgqa@gmail.com>
2021-03-09 03:23:51 +0000

    Cria saque

--- src/index.js
+++ src/index.js
@@ -20,6 +20,18 @@ function verifyIfExistsAccountCpf(request, response, next) {
   return next();
 }
 
+function getBalance(statement) {
+  const balance = statement.reduce((acc, operation) => {
+    if(operation.type === 'credit') {
+      return acc + operation.amount;
+    } else {
+      return acc - operation.amount;
+    }
+  }, 0);
+
+  return balance;
+}
+
 app.use(express.json());
 
 app.post('/account', (request, response) => {
@@ -58,6 +70,27 @@ app.post('/deposit', verifyIfExistsAccountCpf, (request, response) => {
     amount,
     created_at: new Date(),
     type: 'credit'
+  };
+
+  customer.statement.push(statmentOperation);
+
+  return response.status(201).send();
+});
+
+app.post('/withdraw', verifyIfExistsAccountCpf, (request, response) => {
+  const { amount } = request.body;
+  const { customer } = request;
+
+  const balance = getBalance(customer.statement);
+
+  if(balance < amount) {
+    return response.status(400).json({ error: 'Insufficient funds!'});
+  }
+
+  const statmentOperation = {
+    amount,
+    created_at: new Date(),
+    type: 'debit'
   };
 
   customer.statement.push(statmentOperation);
