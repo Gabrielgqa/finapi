d319247c03299181773831ef2a75d6381a5b7e47
GabrielGQA <gabrielgqa@gmail.com>
2021-03-09 03:01:18 +0000

    cria middleware que verifica existência da conta

--- src/index.js
+++ src/index.js
@@ -5,6 +5,21 @@ const app = express();
 
 const customers = [];
 
+// Middleware
+function verifyIfExistsAccountCpf(request, response, next) {
+  const { cpf } = request.headers;
+  const customer = customers.find(customer => customer.cpf === cpf);
+
+  if(!customer) {
+    return response.status(400).json({ error: 'Customer not found!'})
+  }
+
+  // Adicionando novo parâmetro de request no middleware
+  request.customer = customer;
+
+  return next();
+}
+
 app.use(express.json());
 
 app.post('/account', (request, response) => {
@@ -28,13 +43,8 @@ app.post('/account', (request, response) => {
   return response.status(201).send();
 });
 
-app.get('/statement', (request, response) => {
-  const { cpf } = request.headers;
-  const customer = customers.find(customer => customer.cpf === cpf);
-
-  if(!customer) {
-    return response.status(400).json({ error: 'Customer not found!'})
-  }
+app.get('/statement', verifyIfExistsAccountCpf, (request, response) => {
+  const { customer } = request;
 
   return response.json(customer.statement);
 });
