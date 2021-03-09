5bfb3e0de315a2227181095ff2de4c4cd03c6a64
GabrielGQA <gabrielgqa@gmail.com>
2021-03-09 03:30:55 +0000

    Busca extrato pela data

--- src/index.js
+++ src/index.js
@@ -61,6 +61,17 @@ app.get('/statement', verifyIfExistsAccountCpf, (request, response) => {
   return response.json(customer.statement);
 });
 
+app.get('/statement/date', verifyIfExistsAccountCpf, (request, response) => {
+  const { customer } = request;
+  const { date } = request.query;
+
+  const dateFormat = new Date(date + " 00:00");
+
+  const statement = customer.statement.filter((statement) => statement.created_at.toDateString() === new Date(dateFormat).toDateString());
+
+  return response.json(statement);
+});
+
 app.post('/deposit', verifyIfExistsAccountCpf, (request, response) => {
   const { description, amount } = request.body;
   const { customer } = request;
